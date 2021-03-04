56d0190622f5f9b950f73e2ce0c93cc7f64252b1
EricMalpass <ericmalpass@gmail.com>
2021-03-04 15:42:16 -0800

    update

--- HW5java.js
+++ HW5java.js
@@ -1,6 +1,35 @@
+var savedActivity = document.getElementById("activity");
+var saveButton = document.getElementById("save");
+
 var today = moment();
 $("#today").text(today.format("MMM Do, YYYY"));
 
 $( function() {
     $( "#datepicker" ).datepicker();
   } );
+/*
+document.getElementById("myBtn").addEventListener("click", savebtn());
+
+function saveButton() {
+    var x = document.getElementById("activity").value;
+    document.getElementById("savedActivity").innerHTML = x;
+}
+*/
+saveButton.addEventListener("click", function(event) {
+  event.preventDefault();
+
+var savedActivity = {
+    comment: comment.value.trim()
+
+localStorage.setItem("savedActivity", JSON.stringify(savedActivity));
+renderMessage();
+}});
+
+function renderMessage() {
+  var newActivity = JSON.parse(localStorage.getItem("savedActivity"));
+  if (newActivity !== null) {
+    document.querySelector(".message").textContent = newActivity
+  }
+}
+
+
