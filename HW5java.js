e58b524f963bda96e5163e39699a80e681616386
EricMalpass <ericmalpass@gmail.com>
2021-03-05 14:37:12 -0800

    update

--- HW5java.js
+++ HW5java.js
@@ -1,5 +1,5 @@
 var savedActivity = document.getElementById("activity");
-var saveButton = document.getElementById("save");
+var save9 = document.getElementById("save9");
 
 var today = moment();
 $("#today").text(today.format("MMM Do, YYYY"));
@@ -15,22 +15,45 @@ function saveButton() {
     document.getElementById("savedActivity").innerHTML = x;
 }
 */
-saveButton.addEventListener("click", function(event) {
+
+save9.addEventListener("click", function(event) {
   event.preventDefault();
 
-var savedActivity = {
-    comment: comment.value.trim()
-}
+var savedActivity9 = {
+    comment: savedActivity.value}
+    console.log(savedActivity);
 
-localStorage.setItem("savedActivity", JSON.stringify(savedActivity));
+localStorage.setItem("savedActivity", JSON.stringify(savedActivity9));
 renderMessage();
 });
 
 function renderMessage() {
   var newActivity = JSON.parse(localStorage.getItem("savedActivity"));
+  console.log(newActivity.comment)
   if (newActivity !== null) {
-    document.querySelector(".message").textContent = newActivity
+    document.getElementById("activity").textContent = newActivity.comment
   }
+};
+renderMessage();
+
+/*
+var activityEl = $('#activity-input');
+var activityName = $('activityName');
+
+function saveSubmit(event) {
+  event.preventDefault();
+  var activityName = activityEl.val().trim();
 }
 
+activityEl.on('save', saveSubmit);
+*/
+var currentTime = moment().format('HH');
 
+function time() {
+  moment().format('HH');
+  if (9 < currentTime){
+    $("#9am").attr("class", "past")
+  }
+}
+time();
+console.log(currentTime);
