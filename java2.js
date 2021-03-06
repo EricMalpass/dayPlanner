a70c75f97a9ab5809db73f1e87d8ca0988a3482f
EricMalpass <ericmalpass@gmail.com>
2021-03-06 14:38:11 -0800

    update

--- java2.js
+++ java2.js
@@ -1,3 +1,4 @@
+
 var today = moment();
 $("#today").text(today.format("MMM Do, YYYY"));
 
@@ -7,12 +8,16 @@ $( function() {
 
 
 var hours = ["9", "10", "11", "13", "14", "15", "16", "17"];
-var savedActivity = document.getElementById("activity");
+var savedActivity = document.getElementsByClassName("activity");
+var save9 = document.getElementsByClassName("save9");
+var rowtimeEl = document.getElementsByClassName("rowtime")
+console.log(save9);
 
 
-for (var i = 0; i < hours.length ; i++) {
-    save9.addEventListener("click", function(event) {
+for (var i = 0; i < save9.length ; i++) {
+    save9[i].addEventListener("click", function(event) {
         event.preventDefault();
+        console.log("hi")
       
       var savedActivity9 = {
           comment: savedActivity.value}
@@ -21,42 +26,34 @@ for (var i = 0; i < hours.length ; i++) {
       localStorage.setItem("savedActivity", JSON.stringify(savedActivity9));
       renderMessage9();
       });
-      
-      function renderMessage9() {
-        var newActivity = JSON.parse(localStorage.getItem("savedActivity"));
-        console.log(newActivity.comment)
-        if (newActivity !== null) {
-          document.getElementById("activity").textContent = newActivity.comment
-        }
-      };
-    
-}
-
-
-save9.addEventListener("click", function(event) {
-    event.preventDefault();
-  
-  var savedActivity9 = {
-      comment: savedActivity.value}
-      console.log(savedActivity);
-  
-  localStorage.setItem("savedActivity", JSON.stringify(savedActivity9));
-  renderMessage9();
-  });
-  
-  function renderMessage9() {
-    var newActivity = JSON.parse(localStorage.getItem("savedActivity"));
-    console.log(newActivity.comment)
-    if (newActivity !== null) {
-      document.getElementById("activity").textContent = newActivity.comment
     }
-  };
-  renderMessage9();
-  
-  
+    function renderMessage9() {
+      var newActivity = JSON.parse(localStorage.getItem("savedActivity"));
+      console.log(newActivity.comment)
+      if (newActivity !== null) {
+        document.getElementById("activity").textContent = newActivity.comment
+    }
+
   var currentTime = moment().format('HH');
+ 
+  for (let i = 0; i < hours.length; i++) {
+      rowtime = hours[i];
+      function time9() {
+        moment().format('HH');
+        if (hours[i] < currentTime){
+          rowtimeEl.classList.add("class", "past");
+          console.log(currentTime)
+        } else if (hours[i] === currentTime){
+          rowtimeEl[i].classList.add("class", "present");
+        } else if (hours[i]  > currentTime) {
+          rowtimeEl[i].classList.add("class", "future");
+        }
+      }
+      time9();  
+  }
   
-  function time9() {
+
+  //function time9() {
     moment().format('HH');
     if (9 < currentTime){
       $("#9am").attr("class", "past");
@@ -67,4 +64,4 @@ save9.addEventListener("click", function(event) {
       $("#9am").attr("class", "future");
     }
   }
-  time9();
+  //time9();
